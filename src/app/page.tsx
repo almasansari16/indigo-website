207b18064579cfcfb08c5884b1232b1747d5fe6d
almasansari16 <almashanif126@gmail.com>
2024-11-21 16:22:47 +0500

    new changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,9 +29,9 @@ export default function Home() {
       <Sustainability />
       {/* <AboutSectionOne />
       <AboutSectionTwo />
-      <Testimonials />
       <Pricing /> */}
       <Certifications />
+      <Testimonials />
       {/* <Blog /> */}
       <Contact />
     </>
