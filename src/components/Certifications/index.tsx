b13ecd9de5273b382645e137ba47575b6618c3d1
almasansari16 <almashanif126@gmail.com>
2024-11-25 09:54:56 +0500

    responsive

--- src/components/Certifications/index.tsx
+++ src/components/Certifications/index.tsx
@@ -1,3 +1,84 @@
+// 'use client';
+// import React from 'react';
+// import SectionTitle from '../Common/SectionTitle';
+// import Image from 'next/image';
+// import Slider from 'react-slick';
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+
+// const imageData = [
+//   { src: '/images/certificates/Certificate 01.png' },
+//   { src: '/images/certificates/Certificate 02.png' },
+//   { src: '/images/certificates/Certificate 03.png' },
+//   { src: '/images/certificates/Certificate 04.png' },
+//   { src: '/images/certificates/Certificate 05.png' },
+//   { src: '/images/certificates/Certificate 06.png' },
+//   { src: '/images/certificates/Certificate 07.png' },
+//   { src: '/images/certificates/Certificate 08.png' },
+// ];
+
+// const Certifications = () => {
+//   const settings = {
+//     infinite: false,
+//     speed: 500,
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     initialSlide: 0,
+//     autoplay: true,
+//     responsive: [
+//       {
+//         breakpoint: 1024,
+//         settings: {
+//           slidesToShow: 3,
+//           slidesToScroll: 2,
+//           infinite: true,
+//         },
+//       },
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           slidesToShow: 2,
+//           slidesToScroll: 1,
+//         },
+//       },
+//       {
+//         breakpoint: 480,
+//         settings: {
+//           slidesToShow: 1,
+//           slidesToScroll: 1,
+//           arrows: false, // Remove arrows for a cleaner look
+//         },
+//       },
+//     ],
+//   };
+
+//   return (
+//     <div className="container w-full mx-auto py-16 px-4">
+//       <SectionTitle title="Certifications" paragraph="" center />
+//       <Slider {...settings}>
+//           {imageData.map((item, index) => (
+//             <div key={index} className="justify-center items-center">
+//               <div className="relative mx-auto" style={{ height: '700px', width: '700px' }}>
+//                 <Image
+//                   src={item.src}
+//                   alt={`Slide ${index + 1}`}
+//                   layout="fill"
+//                   objectFit="contain"
+//                   quality={100}
+//                   className="rounded-lg "
+//                 />
+//               </div>
+//             </div>
+//           ))}
+//         </Slider>
+//     </div>
+//   );
+// };
+
+// export default Certifications;
+
+
+
 'use client';
 import React from 'react';
 import SectionTitle from '../Common/SectionTitle';
@@ -30,7 +111,7 @@ const Certifications = () => {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 2,
+          slidesToScroll: 1,
           infinite: true,
         },
       },
@@ -53,24 +134,30 @@ const Certifications = () => {
   };
 
   return (
-    <div className="container w-full mx-auto py-16 px-4">
+    <div className="container mx-auto py-16 px-4 flex flex-col items-center">
       <SectionTitle title="Certifications" paragraph="" center />
-      <Slider {...settings}>
+      <div className="w-full max-w-7xl">
+        <Slider {...settings}>
           {imageData.map((item, index) => (
-            <div key={index} className="justify-center items-center">
-              <div className="relative mx-auto" style={{ height: '700px', width: '700px' }}>
+            <div
+              key={index}
+              className="flex justify-center items-center px-4"
+            >
+              <div className="relative max-w-[300px] md:max-w-[500px] lg:max-w-[700px] h-auto">
                 <Image
                   src={item.src}
-                  alt={`Slide ${index + 1}`}
-                  layout="fill"
-                  objectFit="contain"
+                  alt={`Certificate ${index + 1}`}
+                  layout="responsive"
+                  width={500} // Adjust for responsiveness
+                  height={400} // Maintain aspect ratio
                   quality={100}
-                  className="rounded-lg "
+                  className="rounded-lg"
                 />
               </div>
             </div>
           ))}
         </Slider>
+      </div>
     </div>
   );
 };
