32b7febd0c4ef133501357fcdf75b316f1cfe157
almasansari16 <almashanif126@gmail.com>
2024-12-26 10:15:27 -0800

    remove extra pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
 import AboutSectionOne from "@/components/About/AboutSectionOne";
-import AboutSectionTwo from "@/components/About/AboutSectionTwo";
-import Blog from "@/components/Blog";
 import Brands from "@/components/Brands";
 import Certifications from "@/components/Certifications";
 import ScrollUp from "@/components/Common/ScrollUp";
 import Contact from "@/components/Contact";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
-import OurVision from "@/components/ourVision/OurVision";
-import Pricing from "@/components/Pricing";
 import Sustainability from "@/components/Sustaniability";
-import Testimonials from "@/components/Testimonials";
 import Video from "@/components/Video";
 import { Metadata } from "next";
 
@@ -24,17 +19,11 @@ export default function Home() {
     <>
       <ScrollUp />
       <Hero />
-      {/* <OurVision /> */}
       <Features />
       <Video />
       <Brands />
       <Sustainability />
-      {/* <AboutSectionOne />
-      <AboutSectionTwo />
-      <Pricing /> */}
       <Certifications />
-      {/* <Testimonials /> */}
-      {/* <Blog /> */}
       <Contact />
     </>
   );
